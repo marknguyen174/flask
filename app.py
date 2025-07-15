11eebb88f62fee93830ef0629f5210f511a75094
marknguyen174 <anhkhoa83492@gmail.com>
2025-07-15 14:20:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(nguyenvoanhkhoa)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Render!"
+    return "Hello from ANHKHOA!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
